1b3b50498a66d6553ce5c954fd3ed1f95476d661
Alexander Tsarapkine <alexander.tsarapkine@gmail.com>
2022-10-23 23:18:51 -0400

    Added home page gradient background

---
+++ personal-website/src/pages/homePage.js
@@ -0,0 +1,15 @@
+import '../assets/styles/homePage.css';
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <header className="App-header">
+        <p>
+          Hello World!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
